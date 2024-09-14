4807a0f040c68606cb6a213c825422cb5fad7595
radhika-gondaliya <gondaliyaradhika31@.in>
2024-09-14 06:21:26 -0500

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -45,25 +45,31 @@ const AboutUs = () => {
         style={imageStyle}
       />
       <p style={textStyle}>
-        <b style={bold}> Hello, I'm Radhika Gondaliya</b>
+        <h5>
+          {" "}
+          <b style={bold}> Hello, I'm Radhika Gondaliya</b>
+        </h5>
         <br></br>
         <br></br> Since 2021, I've been fortunate to lend my skills to a variety
         of clients, helping them translate their visions into functional,
         aesthetically pleasing websites.<br></br>
         <br></br>I specialize in <b>front-end development</b> , leveraging a
-        stack that includes <b>JavaScript, HTML, CSS, PHP, and more</b> Whether
-        it's crafting a responsive layout, optimizing a site for performance and
-        batter result. My professional journey has been guided by a belief that
-        effective web development is more than just code—it's about
-        understanding the unique needs of each client, creating solutions that
-        address those needs, and ultimately delivering a product that exceeds
-        expectations. <br></br>
+        stack that includes <b>JavaScript, HTML, CSS, react.js,PHP, and more</b>{" "}
+        Whether it's crafting a responsive layout, optimizing a site for
+        performance and batter result. My professional journey has been guided
+        by a belief that effective web development is more than just code—it's
+        about understanding the unique needs of each client, creating solutions
+        that address those needs, and ultimately delivering a product that
+        exceeds expectations. <br></br>
         <br></br>In addition to my technical prowess, I bring strong
         communication skills and a team-focused mentality. I understand that
         every project is a collaborative effort. If you're looking for a
         detail-oriented and committed Web Developer to bring your project to
-        life, feel free to get in touch. I'm always excited about the
-        possibility of creating something great together.
+        life, feel free to get in touch.{" "}
+        <b>
+          I'm always excited about the possibility of creating something great
+          together.
+        </b>
       </p>
 
       <a href="/Radhika resume.pdf" download="Radhika_CV.pdf">
